a7ff7f3aaaeb8e61fbcaac98e880abe093739fab
Amanda Ghassaei <amandaghassaei@gmail.com>
2014-09-08 12:32:42 -0400

    comm radius animation

--- js/agent.js
+++ js/agent.js
@@ -8,8 +8,7 @@ function Agent(xPos, yPos){
     this.color = "#fff";
     this.radius = 10;
     this.position = [xPos, yPos];
-
-
+    this.isAnimatingComm = false;
 }
 
 Agent.prototype.renderAgent = function() {
@@ -35,28 +34,44 @@ Agent.prototype.changeColor = function(color) {
 };
 
 Agent.prototype.transmitData = function() {
-    console.log("amanda");
 };
 
 Agent.prototype.showNetworking = function(){
+    if (this.isAnimatingComm) return;
+    this.isAnimatingComm = true;
+    this.shouldDoAnimation = true;//needed this to help with a timeout-related bug
     var self = this;
     setTimeout(function(){
-        self.animateNeightborCommunication();
-        self.neighborAnimation = setInterval(function(){
-            self.animateNeightborCommunication();
-        }, 3000);
+        if (self.shouldDoAnimation){
+            self.animateNeighborCommunication();
+            self.neighborAnimation = setInterval(function(){
+                if (self.shouldDoAnimation){
+                    self.animateNeighborCommunication();
+                } else {
+                    clearInterval(self.neighborAnimation);
+                    this.isAnimatingComm = false;
+                }
+            }, 3000);
+        }
     }, Math.random()*3000);
-
-//    clearInterval(this.neighborAnimation);
 };
 
-Agent.prototype.animateNeightborCommunication = function() {
+Agent.prototype.hideNetworking = function(){
+    if (this.neighborAnimation){
+        clearInterval(this.neighborAnimation);
+        this.neighborAnimation = null;
+    }
+    this.isAnimatingComm = false;
+    this.shouldDoAnimation = false;
+};
+
+Agent.prototype.animateNeighborCommunication = function() {
     if (!this.neighborhood){
         this.neighborhood = amorphNameSpace.mainCanvas.circle(this.position[0], this.position[1], this.radius);
     }
     this.neighborhood.attr({"stroke-width":"0", "fill-opacity": "0.1"});
     var self = this;
-    this.neighborhood.animate({'fill':'black', 'transform':"s4 4"}, 500, function(){
+    this.neighborhood.animate({'fill':'black', 'transform':"s" + amorphNameSpace.commRadius + " " + amorphNameSpace.commRadius}, 500, function(){
         self.neighborhood.animate({"fill-opacity":"0.0"}, 200, function(){
             self.neighborhood.transform("s1 1");
         });
@@ -68,11 +83,8 @@ Agent.prototype.destroy = function(){
         clearInterval(this.dataConnection);
         this.dataConnection = null;
     }
-    if (this.neighborAnimation) {
-        clearInterval(this.neighborAnimation);
-        this.neighborAnimation = null;
-    }
-    if (this.neighborhood) {
+    this.hideNetworking();
+    if (this.neighborhood){
         this.neighborhood.remove();
         this.neighborhood = null;
     }
--- js/uiControls.js
+++ js/uiControls.js
@@ -20,4 +20,23 @@ $(document).ready(function(){
     });
     numAgents.slider('value',50);//set initial val
 
+    var commRadius = $("#commRadius");
+    commRadius.on("slidestart", function(event, ui){
+        for (var i=0;i<100;i++){//only show a random selection of 100 doing networking - too heavy otherwise
+            amorphNameSpace.agents[i].showNetworking();
+        }
+    });
+    commRadius.on("slidestop", function(event, ui){
+        $.each(amorphNameSpace.agents, function(i, agent) {
+            agent.hideNetworking();
+        });
+    });
+    commRadius.on("slide", function(event, ui){
+        amorphNameSpace.commRadius = ui.value/20.0+2;
+    });
+    commRadius.on("slidechange", function(event, ui){//recalc num agents on slider change
+        amorphNameSpace.commRadius = ui.value/20.0+2;
+    });
+    commRadius.slider('value',30);//set initial val
+
 });
